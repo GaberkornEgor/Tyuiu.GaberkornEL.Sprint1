2c162a9fb177849ff667b8a3ed14db99f7a6a343
GaberkornEL <gaber.egors@gmail.com>
2025-09-29 15:10:23 +0500

    Task3

---
+++ Tyuiu.GaberkornEL.Sprint1.Task3.V15.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GaberkornEL.Sprint1.Task3.V15.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint1.Task3.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double v1 = 20, v2 = 30, S0 = 0, T = 2;
+            double res = ds.DistanceOverTime(v1, v2, S0, T);
+            Assert.AreEqual(100, res);
+
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint1.Task3.V15/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GaberkornEL.Sprint1.Task3.V15.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint1.Task3.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Габеркорн Егор Леонидович  | АСОиУб 25-1";
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                     *");
+            Console.WriteLine("* Тема: Базовые                                                                 *");
+            Console.WriteLine("* Задание #3                                                                    *");
+            Console.WriteLine("* Вариант #15                                                                   *");
+            Console.WriteLine("* Выполнил: Габеркорн Егор Леонидович | АСОиУб 25-1                             *");
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                      *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,       *");
+            Console.WriteLine("* выполняет указанные расчёты и находит расстояние через t часов                *");
+            Console.WriteLine("*                                                                               *");
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                              *");
+            Console.WriteLine("*********************************************************************************");
+
+            double v1, v2, S, T;
+            Console.WriteLine("Введите скорость первого автомобиля V1 =");
+            v1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите скорость второго автомобиля V2 =");
+            v2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите расстояние на котором они первоначально находились S =");
+            S = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите время T =");
+            T = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                    *");
+            Console.WriteLine("*********************************************************************************");
+
+            Console.WriteLine($"Расстояние между автомобилями через {T} часов = {ds.DistanceOverTime(v1, v2, S, T)}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
