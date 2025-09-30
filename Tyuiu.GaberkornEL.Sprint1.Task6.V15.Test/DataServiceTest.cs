ab94e6c091159e7fb19f6dc805a5b9eafcae6137
GaberkornEL <gaber.egors@gmail.com>
2025-09-30 18:44:55 +0500

    Task6

---
+++ Tyuiu.GaberkornEL.Sprint1.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GaberkornEL.Sprint1.Task6.V15.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint1.Task6.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCheckLettersCount()
+        {
+            DataService ds = new DataService();
+            string value = "stropa.";
+            bool result = ds.CheckLettersCount(value);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
